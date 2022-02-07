9c6802b2a6025b9542a54551547dad1fdb644a13
Misha Causur <mishacausur@mail.ru>
2022-02-07 23:05:43 +0500

    Initial Commit

---
+++ combiner/combinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  combinerApp.swift
+//  combiner
+//
+//  Created by Misha Causur on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct combinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
