fe4b7e916aae2b14b9b85731ce2233e92f9196be
Misha Causur <mishacausur@mail.ru>
2022-02-14 22:29:19 +0500

    menu row gradient icon in progress

---
+++ combiner/Views/GradientIcon.swift
@@ -0,0 +1,20 @@
+//
+//  GradientIcon.swift
+//  combiner
+//
+//  Created by Misha Causur on 14.02.2022.
+//
+
+import SwiftUI
+
+struct GradientIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GradientIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientIcon()
+    }
+}
--- combiner/Views/MenuRow.swift
+++ combiner/Views/MenuRow.swift
@@ -13,9 +13,14 @@ struct MenuRow: View {
     var rightIcon: String = "chevron.right"
     var body: some View {
         HStack(spacing: 12) {
+            Image(systemName: leftIcon)
             Text(title)
                 .font(.subheadline)
                 .fontWeight(.semibold)
+            Spacer()
+            Image(systemName: rightIcon)
+                .font(.system(size: 15, weight: .semibold))
+                .opacity(0.3)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
