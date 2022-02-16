cdcf007d814ce843951e352a66f0cfd9824aba42
Misha Causur <mishacausur@mail.ru>
2022-02-16 23:40:53 +0500

    gradient icon in progress

--- combiner/Views/GradientIcon.swift
+++ combiner/Views/GradientIcon.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct GradientIcon: View {
     var iconName: String = "questionmark"
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .orange, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.6196078431, green: 0.6784313725, blue: 1, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.5607843137, blue: 0.9803921569, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .mask {
-                Image(systemName: iconName)
+                Image(systemName: iconName).font(.system(size: 17))
             }
+            .frame(width: 36, height: 36)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1019607843, green: 0.07058823529, blue: 0.2431372549, alpha: 0.6))]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .mask(RoundedRectangle(cornerRadius: 12, style: .continuous))
+            .overlay(RoundedRectangle(cornerRadius: 12, style: .continuous).stroke().foregroundColor(.white).blendMode(.overlay))
     }
 }
 
