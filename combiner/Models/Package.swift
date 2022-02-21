f9d98a267a2873f8da1ca2db2939aef3e9a435ac
Misha Causur <mishacausur@mail.ru>
2022-02-21 21:21:14 +0500

    data model is created

---
+++ combiner/Models/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  combiner
+//
+//  Created by Misha Causur on 21.02.2022.
+//
+
+import Foundation
+
+struct Package: Identifiable {
+    var id = UUID()
+    var title: String
+    var description: String
+}
